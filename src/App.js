ab50ac65b86102e380560fcde05fdff51ddd3f45
rodchen <rodchen_king@outlook.com>
2020-09-27 14:28:05 +0800

    1. useState的初始demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+function Example() {
+  // 声明一个叫 “count” 的 state 变量。
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('rodchen');
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>learn react-hooks</h3>
-      </header>
+    <div>
+      <p>your name: { name }</p>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <button onClick={() => setName('rod chen')}>
+        重置姓名
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Example;

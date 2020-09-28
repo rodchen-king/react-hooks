3f31fd9e7dd09c5a1dc335ff39396fde2cb63796
rodchen <rodchen_king@outlook.com>
2020-09-28 15:33:34 +0800

    1. effect基础demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Example() {
-  // 声明一个叫 “count” 的 state 变量。
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState('rodchen');
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
 
-  return (
-    <div>
-      <p>your name: { name }</p>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-      <button onClick={() => setName(name + ' 11 ')}>
-        重置姓名
-      </button>
-    </div>
-  )
+  componentDidMount() {
+    document.title = `You clicked ${this.state.count} times`;
+  }
+  componentDidUpdate() {
+    document.title = `You clicked ${this.state.count} times`;
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Example;
